d3e2959d7be2fee6d35f8956f97a9ae032e230e6
Miffles <mrk.empz@gmail.com>
2018-03-14 14:14:09 +0800

    Initial commit

---
+++ gulp/gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var pug = require('gulp-pug');
+var plumber = require('gulp-plumber');
+
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'pug'], function () {
+
+  browserSync.init({
+    server: "./app"
+  });
+
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('app/pug/*.pug', ['pug']);
+  gulp.watch('app/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
+
+gulp.task('pug', function () {
+  return gulp.src('app/pug/*.pug')
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true,
+    }))
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('app'));
+});
